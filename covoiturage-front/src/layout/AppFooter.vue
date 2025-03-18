0551e21702ce646b46dfc96eddde6316f49b32f6
oumaimazaddam <oumaimazaddem817@gmail.com>
2025-03-18 11:51:54 -0400

    [Ajouter page]Messengers

--- covoiturage-front/src/layout/AppFooter.vue
+++ covoiturage-front/src/layout/AppFooter.vue
@@ -1,8 +1,5 @@
+<!-- eslint-disable vue/valid-template-root -->
 <script setup></script>
 
-<template>
-    <div class="layout-footer">
-        SAKAI by
-        <a href="https://primevue.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">PrimeVue</a>
-    </div>
-</template>
+// eslint-disable-next-line vue/valid-template-root
+<template></template>
